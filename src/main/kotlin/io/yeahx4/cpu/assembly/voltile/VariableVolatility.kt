e2d21ca11fca315b168b781c6633cd071bb3974e
YEAHx4 <developerstarlight@gmail.com>
2023-05-30 22:51:36 +0900

    Auto managed variable assignment

--- src/main/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatility.kt
+++ src/main/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatility.kt
@@ -1,5 +1,6 @@
 package io.yeahx4.cpu.assembly.voltile
 
+import io.yeahx4.cpu.assembly.MemoryOutOfBoundException
 import io.yeahx4.cpu.assembly.VariableNotFoundException
 import io.yeahx4.cpu.logic.VByte
 import io.yeahx4.cpu.memory.structure.ByteRam
@@ -16,12 +17,62 @@ class VariableVolatility private constructor(){
     // VariableName, VariableAddress
     private val nameTable = mutableMapOf<String, VByte>()
 
-    fun assign(name: String, addr: VByte, value: VByte) {
+    private fun getFreeAddr(length: Int = 1): VByte {
+        var index = 0
+        var destination = -1
+
+        while (index <= VByte.MAX_VALUE && destination == -1) {
+            if (this.ram.get(index) == null) {
+                var good = true
+                var offset = 1
+
+                while (length != 1 && good) {
+                    if (
+                        index + offset > VByte.MAX_VALUE
+                        || this.ram.get(index + offset) != null
+                    ) {
+                        good = false
+                    }
+
+                    if (offset + 1 == length) {
+                        break
+                    }
+
+                    offset++
+                }
+
+                if (good) {
+                    destination = if (length == 1)
+                        index
+                    else
+                        index + offset
+
+                    break
+                }
+            }
+
+            index++
+        }
+
+        // No Free Memory Space
+        if (index > VByte.MAX_VALUE && destination == -1)
+            throw MemoryOutOfBoundException()
+
+        return VByte.fromDec(destination)
+    }
+
+    fun assign(name: String, value: VByte) {
+        val addr = getFreeAddr()
+
         if (nameTable.containsKey(name))
             throw VariableRedeclaredException(name)
 
         nameTable[name] = addr.duplicate()
         ram.write(addr.duplicate(), value.duplicate())
+    }
+
+    fun assign(name: String, value: Int) {
+        this.assign(name, VByte.fromDec(value))
     }
 
     fun modify(name: String, value: VByte) {
@@ -31,11 +82,23 @@ class VariableVolatility private constructor(){
         this.ram.modify(this.nameTable[name]!!, value.duplicate())
     }
 
+    fun modify(name: String, value: Int) {
+        this.modify(name, VByte.fromDec(value))
+    }
+
     fun read(name: String): VByte {
         if (!nameTable.containsKey(name))
             throw VariableNotFoundException(name)
 
         return this.ram.get(this.nameTable[name]!!)!!
+    }
+
+    fun delete(name: String) {
+        if (!nameTable.containsKey(name))
+            throw VariableNotFoundException(name)
+
+        this.ram.erase(this.nameTable[name]!!)
+        this.nameTable.remove(name)
     }
 
     fun destroy() {
--- src/test/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatilityTest.kt
+++ src/test/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatilityTest.kt
@@ -1,4 +1,61 @@
 package io.yeahx4.cpu.assembly.voltile
 
+import io.yeahx4.cpu.assembly.VariableNotFoundException
+import io.yeahx4.cpu.logic.VByte
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
 class VariableVolatilityTest {
+    @BeforeEach
+    fun `Clear memory`() {
+        VariableVolatility
+            .getInstance()
+            .destroy()
+    }
+
+    @Test
+    fun `Assign Value and Read`() {
+        val volatility = VariableVolatility.getInstance()
+
+        volatility.assign("a", VByte.fromDec(10))
+        volatility.assign("b", VByte.fromDec(10))
+        volatility.assign("c", VByte.fromDec(10))
+
+        assert(volatility.read("a").toDec() == 10)
+    }
+
+    @Test
+    fun `Modify value`() {
+        val volatility = VariableVolatility.getInstance()
+
+        volatility.assign("a", 10)
+        volatility.modify("a", 20)
+
+        assert(volatility.read("a").toDec() == 20)
+    }
+
+    @Test
+    fun `Delete value`() {
+        val volatility = VariableVolatility.getInstance()
+
+        volatility.assign("a", 10)
+        volatility.assign("b", 20)
+        volatility.assign("c", 30)
+
+        volatility.delete("b")
+
+        volatility.assign("d", 40)
+        volatility.assign("e", 50)
+
+        assert(volatility.read("a").toDec() == 10)
+        assert(volatility.read("c").toDec() == 30)
+        assert(volatility.read("d").toDec() == 40)
+        assert(volatility.read("e").toDec() == 50)
+
+        try {
+            volatility.read("b")
+            fail("No exception detected.")
+        } catch (_: VariableNotFoundException) {}
+    }
 }
