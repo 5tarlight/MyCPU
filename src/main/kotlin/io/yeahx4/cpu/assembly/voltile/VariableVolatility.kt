c06111d1fa71c59e62eb43c72524e3f27cf29d28
YEAHx4 <developerstarlight@gmail.com>
2023-05-30 23:25:00 +0900

    Volatility documentation

--- src/main/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatility.kt
+++ src/main/kotlin/io/yeahx4/cpu/assembly/voltile/VariableVolatility.kt
@@ -6,10 +6,30 @@ import io.yeahx4.cpu.logic.VByte
 import io.yeahx4.cpu.memory.structure.ByteRam
 import io.yeahx4.cpu.assembly.VariableRedeclaredException
 
+/**
+ * Automatic `VByte` based memory (similar to RAM)
+ * management system.
+ * Every constructor is private and this class is
+ * designed to be used in Singleton.
+ * Note that this class share one instance with
+ * every reference, which means adding, reading,
+ * modifying value will affect every reference of
+ * this class.
+ *
+ * @see VByte
+ * @since 1.0
+ */
 class VariableVolatility private constructor(){
     companion object {
+        /**
+         * Shared instance of itself
+         */
         private val instance = VariableVolatility()
 
+        /**
+         * Get instance of `VariableVolatility`.
+         * This instance is shared in universe.
+         */
         fun getInstance() = instance
     }
 
@@ -61,6 +81,13 @@ class VariableVolatility private constructor(){
         return VByte.fromDec(destination)
     }
 
+    /**
+     * Assign new named variable with value.
+     * An address where new variable will be stored is
+     * automatically determined by system.
+     *
+     * @see assign
+     */
     fun assign(name: String, value: VByte) {
         val addr = getFreeAddr()
 
@@ -71,10 +98,22 @@ class VariableVolatility private constructor(){
         ram.write(addr.duplicate(), value.duplicate())
     }
 
+    /**
+     * Assign new named variable with value
+     * in decimal.
+     * Wrapper of assign(String, VByte)
+     *
+     * @see assign
+     */
     fun assign(name: String, value: Int) {
         this.assign(name, VByte.fromDec(value))
     }
 
+    /**
+     * Alter value of variable which is already declared.
+     *
+     * @throws VariableNotFoundException name of the variable is unknown.
+     */
     fun modify(name: String, value: VByte) {
         if (!nameTable.containsKey(name))
             throw VariableNotFoundException(name)
@@ -82,10 +121,21 @@ class VariableVolatility private constructor(){
         this.ram.modify(this.nameTable[name]!!, value.duplicate())
     }
 
+    /**
+     * Alter value of variable with decimal value.
+     * Wrapper of modify(String, VByte)
+     *
+     * @throws VariableNotFoundException name of the variable is unknown.
+     */
     fun modify(name: String, value: Int) {
         this.modify(name, VByte.fromDec(value))
     }
 
+    /**
+     * Read value of variable with name.
+     *
+     * @throws VariableNotFoundException name of the variable is unknown.
+     */
     fun read(name: String): VByte {
         if (!nameTable.containsKey(name))
             throw VariableNotFoundException(name)
@@ -93,6 +143,13 @@ class VariableVolatility private constructor(){
         return this.ram.get(this.nameTable[name]!!)!!
     }
 
+    /**
+     * Remove variable from the memory.
+     * After deletion, memory is freed and ready for
+     * new variable assignment.
+     *
+     * @throws VariableNotFoundException name of the variable is unknown.
+     */
     fun delete(name: String) {
         if (!nameTable.containsKey(name))
             throw VariableNotFoundException(name)
@@ -101,6 +158,9 @@ class VariableVolatility private constructor(){
         this.nameTable.remove(name)
     }
 
+    /**
+     * Destroy the memory and delete everything.
+     */
     fun destroy() {
         this.ram.clear()
         this.nameTable.clear()
